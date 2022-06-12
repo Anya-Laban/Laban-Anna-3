c74b04fe6d4aa8f0b17ac95111f2c888e473d6a2
Anya-Laban <laban.anya.a@gmail.com>
2022-06-12 15:39:12 +0300

    Laban-Anna-3

---
+++ ConsoleApp3.2/Hemlet.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ConsoleApp3._2
+{
+    class Helmet : Amunition
+    {
+        public override string Name { get; protected set; } = "Helmet";
+        public override double Weight { get; protected set; } = 1.30;
+        public override double Price { get; protected set; } = 300;
+        public override string Size { get; protected set; } = "L";
+        private bool Glass = false;
+        public string Color { get; private set; } = "Black";
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Glass: " + Glass);
+            Console.WriteLine("Color" + Color);
+        }
+    }
+}
